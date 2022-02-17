d633d09c942e5bdd70c637246bb72bd672bfdfed
G B Aswath <gbaswath@zohomail.in>
2022-02-17 07:51:42 +0530

    Method Reference Types Example

--- functional-interface/src/main/java/io/programming/chapter7/item42/functional/lambda/MethodReferenceExample.java
+++ functional-interface/src/main/java/io/programming/chapter7/item42/functional/lambda/MethodReferenceExample.java
@@ -1,9 +1,11 @@
 package io.programming.chapter7.item42.functional.lambda;
 
+import java.time.Instant;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.function.Function;
 
 /**
@@ -67,6 +69,46 @@ public class MethodReferenceExample {
         } else {
             elements.forEach(System.out::println);
         }
+    }
+
+    /**
+     * Check if current time is greater than given time instant
+     *
+     * @param time - Time Instant to check against current time.
+     * @return true - if current time is greater else false
+     */
+    static boolean checkIfCurrentTimeGreaterBoundedMethodReference(Instant time) {
+        return Optional.ofNullable(time).filter(Instant.now()::isAfter).isPresent();
+    }
+
+    /**
+     * Compute Length of given string input
+     *
+     * @param input - String length to be computed
+     * @return number of characters count if not empty else null
+     */
+    static int getStringLengthUsingUnboundMethodReference(String input) {
+        return Optional.ofNullable(input).map(String::length).orElse(0);
+    }
+
+    /**
+     * Gets Empty String incase of <code>null</code> input.
+     *
+     * @param input - Input to be checked for <code>null</code>.
+     * @return input if it is not <code>null</code> else return empty
+     *         <code>String</code>
+     */
+    static String getStringInstanceIfEmptyUsingConstructorMethodReference(String input) {
+        return Optional.ofNullable(input).orElseGet(String::new);
+    }
+
+    /**
+     * Create Array Instance basis input length
+     *
+     * @param input - array length
+     */
+    static int[] createArrayInstanceUsingConstructorMethodReference(Integer input) {
+        return Optional.ofNullable(input).filter(value -> value > 0).map(int[]::new).orElse(null);
     }
 
 }
--- functional-interface/src/test/java/io/programming/chapter7/item42/functional/lambda/MethodReferenceExampleTest.java
+++ functional-interface/src/test/java/io/programming/chapter7/item42/functional/lambda/MethodReferenceExampleTest.java
@@ -1,5 +1,6 @@
 package io.programming.chapter7.item42.functional.lambda;
 
+import java.time.Instant;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
@@ -60,12 +61,79 @@ public class MethodReferenceExampleTest {
         MethodReferenceExample.printElementsUsingLambda(elements);
     }
 
+    @Test(dataProvider = "getTimeInstantDataProvider")
+    public void testCheckIfCurrentTimeGreaterBoundedMethodReference(
+            Instant inputTime, boolean expected) {
+        Assert.assertEquals(
+                MethodReferenceExample.checkIfCurrentTimeGreaterBoundedMethodReference(inputTime),
+                expected);
+    }
+
+    @Test(dataProvider = "getStringCountDataProvider")
+    public void testGetStringLengthUsingUnboundMethodReference(String input, int expectedCount) {
+        Assert.assertEquals(
+                MethodReferenceExample.getStringLengthUsingUnboundMethodReference(input), expectedCount);
+    }
+
+    @Test(dataProvider = "getStringInstanceDataProvider")
+    public void testGetStringInstanceIfEmptyUsingConstructorMethodReference(
+            String input, String expectedOutput) {
+        Assert.assertEquals(
+                MethodReferenceExample.getStringInstanceIfEmptyUsingConstructorMethodReference(input),
+                expectedOutput);
+    }
+
+    @Test(dataProvider = "getIntegerDataProvider")
+    public void testCreateArrayInstanceUsingConstructorMethodReference(Integer input, int[] expected) {
+        int[] actual = MethodReferenceExample.createArrayInstanceUsingConstructorMethodReference(input);
+        Assert.assertEquals(actual, expected);
+    }
+
     @DataProvider
     public Object[][] getElementsDataProvider() {
         return new Object[][] {
-            new Object[] {null},
-            new Object[] {Collections.emptyList()},
-            new Object[] {Arrays.asList(null, "HI", null)},
+                new Object[] { null },
+                new Object[] { Collections.emptyList() },
+                new Object[] { Arrays.asList(null, "HI", null) },
         };
     }
+
+    @DataProvider
+    public static Object[][] getTimeInstantDataProvider() {
+        return new Object[][] {
+                new Object[] { null, false },
+                new Object[] { Instant.now().plusSeconds(100), false },
+                new Object[] { Instant.now(), true },
+                new Object[] { Instant.now().minusSeconds(100), true },
+        };
+    }
+
+    @DataProvider
+    public static Object[][] getStringCountDataProvider() {
+        return new Object[][] {
+                new Object[] { null, 0 },
+                new Object[] { "", 0 },
+                new Object[] { "null", 4 },
+                new Object[] { "String", 6 },
+                new Object[] { " String ", 8 }
+        };
+    }
+
+    @DataProvider
+    public static Object[][] getStringInstanceDataProvider() {
+        return new Object[][] {
+                new Object[] { null, "" },
+                new Object[] { "", "" },
+                new Object[] { "null", "null" },
+                new Object[] { "HI", "HI" },
+        };
+    }
+
+    @DataProvider
+    public static Object[][] getIntegerDataProvider() {
+        return new Object[][] {
+                new Object[] { null, null }, new Object[] { 5, new int[5] }, new Object[] { -1, null },
+        };
+    }
+
 }
