eed45ba807a3cdfb601f0bec9f93c0bb94610fe3
G B Aswath <gbaswath@zohomail.in>
2022-03-03 17:36:58 +0530

    Reuse of Stream Elements
    1. StreamExample.java -> Get Primes using Mersenne Number
    2. StreamExampleTest.java -> Test Get Primes Test Code.

--- functional-interface/src/main/java/io/programming/chapter7/item42/functional/lambda/StreamExample.java
+++ functional-interface/src/main/java/io/programming/chapter7/item42/functional/lambda/StreamExample.java
@@ -1,6 +1,10 @@
 package io.programming.chapter7.item42.functional.lambda;
 
+import java.math.BigInteger;
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * Item 45: Use Streams Judisiously
@@ -32,6 +36,7 @@ public class StreamExample {
 
     /**
      * Sort Characters in String using Arrays.sort
+     * 
      * @param word
      * @return
      */
@@ -65,8 +70,10 @@ public class StreamExample {
     }
 
     /**
-     * Collect has Supplier -> StringBuilder::new, Accumulator -> StringBuilder.append
+     * Collect has Supplier -> StringBuilder::new, Accumulator ->
+     * StringBuilder.append
      * & Combiner -> StringBuilder::append
+     * 
      * @param word1 - Input Word
      * @return Sorted Word
      */
@@ -74,6 +81,46 @@ public class StreamExample {
         return word1.chars().sorted().collect(StringBuilder::new,
                 (sb, c) -> sb.append((char) c), StringBuilder::append)
                 .toString();
+    }
+
+    /**
+     * Get Primes basis Mersenne Prime
+     * 
+     * @return
+     */
+    static List<BigInteger> getPrimes() {
+        // Start of Stream is 2 and along with next prime number generator function
+        Stream<BigInteger> probablePrimes = Stream.iterate(BigInteger.valueOf(2), BigInteger::nextProbablePrime);
+        return probablePrimes
+                // Mersenne Prime is of form 2^prime - 1 will also be prime. So map prime to
+                // mersenne prime
+                .map(prime -> BigInteger.valueOf(2).pow(prime.intValue()).subtract(BigInteger.ONE))
+                // Filter value if it is prime by checking probabilistic primality test by using
+                // magic number 50.
+                .filter(prime -> prime.isProbablePrime(50))
+                // Limit to 10 count.
+                .limit(10)
+                // Collect result into List
+                .collect(Collectors.toList());
+    }
+
+    /**
+     * Generate prime numbers which are also mersenne prime.
+     * 
+     * @return List of numbers that are responsible for generating mersenne prime.
+     */
+    static List<Integer> getInputPrimesAfterStreamElementsReuse() {
+        Stream<BigInteger> probablePrimes = Stream.iterate(BigInteger.valueOf(2), BigInteger::nextProbablePrime);
+        return probablePrimes
+                // Mersenne Prime is of form 2^prime - 1 will also be prime. So map prime to
+                // mersenne prime
+                .map(prime -> BigInteger.valueOf(2).pow(prime.intValue()).subtract(BigInteger.ONE))
+                // Invert mapping to get the exponent of mersenne prime
+                .map(BigInteger::bitLength)
+                // limit it to 10 count
+                .limit(10)
+                // Collect result into List
+                .collect(Collectors.toList());
     }
 
 }
--- functional-interface/src/test/java/io/programming/chapter7/item42/functional/lambda/StreamExampleTest.java
+++ functional-interface/src/test/java/io/programming/chapter7/item42/functional/lambda/StreamExampleTest.java
@@ -1,5 +1,8 @@
 package io.programming.chapter7.item42.functional.lambda;
 
+import java.math.BigInteger;
+import java.util.Collection;
+
 import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
@@ -8,11 +11,12 @@ import org.testng.annotations.Test;
  * StreamExample Test Code
  */
 public class StreamExampleTest {
-    
+
     /**
      * Test for IsAnagram using Iterative API
-     * @param word1 - Input1
-     * @param word2 - Input 2
+     * 
+     * @param word1    - Input1
+     * @param word2    - Input 2
      * @param expected - Expected Value from Dataprovider.
      */
     @Test(dataProvider = "getAnagramDataProvider")
@@ -22,8 +26,9 @@ public class StreamExampleTest {
 
     /**
      * Test for IsAnagram using Stream API
-     * @param word1 - Input1
-     * @param word2 - Input 2
+     * 
+     * @param word1    - Input1
+     * @param word2    - Input 2
      * @param expected - Expected Value from Dataprovider.
      */
     @Test(dataProvider = "getAnagramDataProvider")
@@ -34,12 +39,26 @@ public class StreamExampleTest {
     @DataProvider
     public Object[][] getAnagramDataProvider() {
         return new Object[][] {
-            new Object[] {null, null, false},
-            new Object[] {null, "a", false},
-            new Object[] {"b", null, false},
-            new Object[] {"petals", "staple", true},
-            new Object[] {"etals", "taple", false},
+                new Object[] { null, null, false },
+                new Object[] { null, "a", false },
+                new Object[] { "b", null, false },
+                new Object[] { "petals", "staple", true },
+                new Object[] { "etals", "taple", false },
         };
+    }
+
+    @Test
+    public void testGetPrimes() {
+        Collection<BigInteger> primes = StreamExample.getPrimes();
+        System.out.println("Got Primes" + primes);
+        Assert.assertEquals(primes.size(), 10);
+    }
+
+    @Test
+    public void testGetInputPrimesAfterStreamElementsReuse() {
+        Collection<Integer> primes = StreamExample.getInputPrimesAfterStreamElementsReuse();
+        System.out.println("Got Primes" + primes);
+        Assert.assertEquals(primes.size(), 10);
     }
 
 }
