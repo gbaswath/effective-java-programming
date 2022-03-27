df99b1aedbf2e7c40953dff9ff17e0f45cfb53d0
G B Aswath <gbaswath@zohomail.in>
2022-03-27 07:01:01 +0530

    Use Int / Long apart from BigDecimal in case of inconvenience.
    1. BigDecimalExample -> Use Notes and Coins separately through int/long datatype in case of BD is not suitable due to performance considerations
    2. BigDecimalExampleTest -> Test through Int Quantity

--- functional-interface/src/main/java/io/programming/chapter9/item57/general/programming/BigDecimalExample.java
+++ functional-interface/src/main/java/io/programming/chapter9/item57/general/programming/BigDecimalExample.java
@@ -47,10 +47,64 @@ public class BigDecimalExample {
     static BigDecimal findRemainingExact(String initial, String used) {
         BigDecimal initialBD = new BigDecimal(initial);
         BigDecimal usedBD = new BigDecimal(used);
-        if (usedBD.compareTo(initialBD) <=0) {
+        if (usedBD.compareTo(initialBD) <= 0) {
             return initialBD.subtract(usedBD);
         } else {
             return BigDecimal.valueOf(0);
         }
+    }
+
+    /**
+     * Find Remaining Amount after purchasing items with each of its purchase value
+     * 
+     * @param initial               - Amount had before purchasing items
+     * @param purchasedItemQuantity - Item count to be purchased
+     * @param priceForEachItem      - Price of each item
+     * @return Amount after purchasing items
+     */
+    static float findRemaining(float initial, int purchasedItemQuantity, float priceForEachItem) {
+        return initial - (purchasedItemQuantity * priceForEachItem);
+    }
+
+    /**
+     * Find Remaining Amount after purchasing items with each of its purchase value
+     * 
+     * @param initial               - Amount had before purchasing items
+     * @param purchasedItemQuantity - Item count to be purchased
+     * @param priceForEachItem      - Price of each item
+     * @return Amount after purchasing items
+     */
+    static double findRemaining(double initial, int purchasedItemQuantity, double priceForEachItem) {
+        return initial - (purchasedItemQuantity * priceForEachItem);
+    }
+
+    /**
+     * Find Remaining Amount after purchasing items with each of its purchase value
+     * 
+     * @param initial               - Amount had before purchasing items
+     * @param purchasedItemQuantity - Item count to be purchased
+     * @param priceForEachItem      - Price of each item
+     * @return Amount after purchasing items
+     */
+    static BigDecimal findRemainingExact(String initial, int purchasedItemQuantity, String priceForEachItem) {
+        return new BigDecimal(initial)
+                .subtract(new BigDecimal(priceForEachItem).multiply(BigDecimal.valueOf(purchasedItemQuantity)));
+    }
+
+    /**
+     * Find Remaining Amount after purchasing items with each of its purchase value
+     * 
+     * @param notes                 - Amount had before purchasing items
+     * @param coins                 - Coins has before purchasing items
+     * @param purchasedItemQuantity - Item count to be purchased
+     * @param priceForEachItem      - Price of each item
+     * @return Amount after purchasing items
+     */
+    static int findRemainingExact(int notes, int coins, int purchasedItemQuantity, int priceForEachItem) {
+        int remaining = notes * 100 + coins;
+        for (int i = 0; i < purchasedItemQuantity; i++) {
+            remaining = remaining - priceForEachItem;
+        }
+        return remaining;
     }
 }
--- functional-interface/src/test/java/io/programming/chapter9/item57/general/programming/BigDecimalExampleTest.java
+++ functional-interface/src/test/java/io/programming/chapter9/item57/general/programming/BigDecimalExampleTest.java
@@ -45,7 +45,7 @@ public class BigDecimalExampleTest {
         System.out.println("[BigDecimal] Test Remaining with Initial " + initial + " & Used " + used);
         System.out.println("[BigDecimal] Got " + BigDecimalExample.findRemainingExact(initial, used));
     }
-   
+
     @DataProvider
     public Object[][] getString() {
         return new Object[][] {
@@ -54,6 +54,85 @@ public class BigDecimalExampleTest {
                 new Object[] { "1.04", "0.4" },
                 new Object[] { "1.0", "0.43" },
                 new Object[] { "1.04", "0.43" },
+        };
+    }
+
+    @Test(dataProvider = "getFloatQuantity")
+    public void testRemainingAmountQuantityFloat(float initial, int purchasedItemQuantity, float priceForEachItem) {
+        System.out.println("[Float] Test Remaining with Initial " + initial + " Quantity Purchased "
+                + purchasedItemQuantity + " with price " + priceForEachItem);
+        System.out.println(
+                "[Float] Got " + BigDecimalExample.findRemaining(initial, purchasedItemQuantity, priceForEachItem));
+    }
+
+    @DataProvider
+    public Object[][] getFloatQuantity() {
+        return new Object[][] {
+                new Object[] { 0f, 0, 0f },
+                new Object[] { 1f, 9, 0.1f },
+                new Object[] { 5.2f, 4, 0.10f },
+                new Object[] { 1.03f, 2, 0.47f },
+                new Object[] { 1.3f, 2, 0.44f },
+        };
+    }
+
+    @Test(dataProvider = "getDoubleQuantity")
+    public void testRemainingAmountQuantityDouble(double initial, int purchasedItemQuantity, double priceForEachItem) {
+        System.out.println("[Double] Test Remaining with Initial " + initial + " Quantity Purchased "
+                + purchasedItemQuantity + " with price " + priceForEachItem);
+        System.out.println(
+                "[Double] Got " + BigDecimalExample.findRemaining(initial, purchasedItemQuantity, priceForEachItem));
+    }
+
+    @DataProvider
+    public Object[][] getDoubleQuantity() {
+        return new Object[][] {
+                new Object[] { 0d, 0, 0d },
+                new Object[] { 1d, 9, 0.1d },
+                new Object[] { 8.3d, 4, 0.17d },
+                new Object[] { 1.03d, 2, 0.47d },
+                new Object[] { 1.3d, 2, 0.44d },
+        };
+    }
+
+    @Test(dataProvider = "getBigDecimalQuantity")
+    public void testRemainingAmountQuantityExactBD(String initial, int purchasedItemQuantity, String priceForEachItem) {
+        System.out.println("[BD] Test Remaining with Initial " + initial + " Quantity Purchased "
+                + purchasedItemQuantity + " with price " + priceForEachItem);
+        System.out.println(
+                "[BD] Got " + BigDecimalExample.findRemainingExact(initial, purchasedItemQuantity, priceForEachItem));
+    }
+
+    @DataProvider
+    public Object[][] getBigDecimalQuantity() {
+        return new Object[][] {
+                new Object[] { "0", 0, "0" },
+                new Object[] { "1.0", 9, "0.1" },
+                new Object[] { "8.3", 4, "0.17" },
+                new Object[] { "1.03", 2, "0.47" },
+                new Object[] { "1.3", 2, "0.44" },
+        };
+    }
+
+    @Test(dataProvider = "getIntQuantity")
+    public void testRemainingAmountQuantityExactInt(int notes, int coins, int purchasedItemQuantity,
+            int priceForEachItem) {
+        System.out.println("[Int] Test Remaining with Initial " + notes + " Coins " + coins + " Quantity Purchased "
+                + purchasedItemQuantity + " with price " + priceForEachItem);
+        System.out.println(
+                "[Int] Got "
+                        + BigDecimalExample.findRemainingExact(notes, coins, purchasedItemQuantity, priceForEachItem)
+                        + " coins");
+    }
+
+    @DataProvider
+    public Object[][] getIntQuantity() {
+        return new Object[][] {
+                new Object[] { 0, 0, 0, 0 },
+                new Object[] { 1, 0, 9, 10 },
+                new Object[] { 8, 30, 4, 17 },
+                new Object[] { 1, 3, 2, 47 },
+                new Object[] { 1, 30, 2, 44 },
         };
     }
 }
